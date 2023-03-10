577d61fc6d3862ca0575284a1f3fb4dbe188d981
reytler <reytlercompare@hotmail.com>
2023-03-10 09:32:16 -0300

    finished layout

--- src/components/App/index.tsx
+++ src/components/App/index.tsx
@@ -1,5 +1,5 @@
 import { GlobalStyles } from "../../assets/styles/global";
-import { Button, Conteiner, Controlsarea, H3, Textarea } from "./style";
+import { Btnssarea, Button, Conteiner, Controlsarea, H3, Textarea } from "./style";
 import {FaPlay, FaPause, FaStop} from 'react-icons/fa';
 
 function App() {
@@ -12,20 +12,42 @@ function App() {
       <Textarea>
         <textarea rows={15} cols={33} className="inputtext"/>
       </Textarea>
-      <Controlsarea>
-        <Button onClick={()=>console.log('Clicou...')}>
-          <FaPlay/>
-        </Button>
-        <Button onClick={()=>console.log('Clicou...')}>
-          <FaPause/>
-        </Button>
-        <Button onClick={()=>console.log('Clicou...')}>
-        <FaPause/><FaPlay/>
-        </Button>
-        <Button onClick={()=>console.log('Clicou...')}>
-          <FaStop/>
-        </Button>
-      </Controlsarea>
+      <div className="controlsarea">
+        <Controlsarea>
+          <div className="divslider">
+            <span>
+              Velocidade:
+            </span>
+            <input type="range" min={0} max={2} step={0.1}/>
+          </div>
+          <div className="divslider">
+            <span>
+              Volume:
+            </span>
+            <input type="range" min={0} max={2} step={0.1}/>
+          </div>
+          <div className="divslider">
+            <span>
+              Afinação:
+            </span>
+            <input type="range" min={0} max={2} step={0.1}/>
+          </div>
+        </Controlsarea>
+        <Btnssarea>
+          <Button onClick={()=>console.log('Clicou...')}>
+            <FaPlay/>
+          </Button>
+          <Button onClick={()=>console.log('Clicou...')}>
+            <FaPause/>
+          </Button>
+          <Button onClick={()=>console.log('Clicou...')}>
+            <FaPause className="resume"/><FaPlay/>
+          </Button>
+          <Button onClick={()=>console.log('Clicou...')}>
+            <FaStop/>
+          </Button>
+        </Btnssarea>
+      </div>
     </Conteiner>
   );
 }
--- src/components/App/style.ts
+++ src/components/App/style.ts
@@ -2,6 +2,13 @@ import styled from "styled-components";
 
 export const Conteiner = styled.div`
     height: 100%;
+
+    .controlsarea{
+        display: flex;
+        flex-direction: column;
+        justify-content: center;
+        align-items: center;
+    }
 `;
 
 export const H3 = styled.h3`
@@ -32,16 +39,79 @@ export const Textarea = styled.div`
     }
 `;
 
-export const Controlsarea = styled.div``;
-
-export const Button = styled.span`
+export const Btnssarea = styled.div`
     display: flex;
-    color: rgba(102,52,127,1);
-    border: none;
-    cursor: pointer;
-    background: transparent;
+    justify-content: space-between;
+    width: 350px;
+    margin: 15px 0 15px 0;
+    .resume{
+        margin-right: -6px;
+    }
+`;
 
-    &:focus{
-        border: rgba(102,52,127,1) !important;
+export const Controlsarea = styled.div`
+
+    margin: 15px 0 15px 0;
+
+    input[type='range'] {
+        cursor: pointer;
+    }
+
+    input[type='range'],
+    input[type='range']::-webkit-slider-runnable-track,
+    input[type='range']::-webkit-slider-thumb {
+        -webkit-appearance: none;
+    }
+
+    input[type='range']::-webkit-slider-runnable-track {
+        width: 200px;
+        height: 10px;
+        background: rgba(102, 52, 127,0.5);
+        border-radius: 5px;
+    }
+
+    input[type='range']::-webkit-slider-thumb {
+        width: 20px;
+        height: 20px;
+        margin-top: -4.5px;
+        background-color: rgb(102, 52, 127);
+        border-radius: 60px;
+        border: 1px solid #66347F;
+    }
+
+    span{
+        color: rgba(102, 52, 127,0.9);
+        font-size: 16px;
+        font-weight: 600;
+        line-height: 25px;
+    }
+
+    .divslider{
+        display: flex;
+        justify-content: space-between;
+        align-items: center;
+        width: 350px;
+    }
+`;
+
+export const Button = styled.button`
+
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    align-content: center;
+    color: rgba(102,52,127,1);
+    background: transparent;
+    width: 40px;
+    height: 40px;
+    cursor: pointer;
+
+    padding: 7px;
+
+    border: solid 1px rgba(102,52,127,0.5);
+    border-radius: 5px;
+
+    &:active{
+        border: 1px solid rgba(102,52,127,1);
     }
 `;
